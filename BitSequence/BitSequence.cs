0001dbf0502ae6c3f622e0ecae0ebaa84e4a0940
Inego <yanis.batura@gmail.com>
2015-03-21 11:56:19 +0600

    Initial commit

---
+++ BitSequence/BitSequence.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSequence
+{
+    class BitSequence
+    {
+        private int[] bits = new int[10];
+        private byte currentInt = 0;
+        private int currentBit = 0;
+
+        private string IntToBit(byte pos)
+        {
+            return Convert.ToString(bits[pos], 2).PadLeft(32, '0');
+        }
+
+        public void Reset()
+        {
+            bits[0] = 0;
+            currentBit = 0;
+            currentInt = 0;
+        }
+        
+        public void AddBits(int bitContainer, byte bitsToAdd)
+        {
+            if (currentBit == 0)
+                bits[currentInt] = bitContainer;
+            else
+                bits[currentInt] |= bitContainer << currentBit;
+
+            currentBit += bitsToAdd;
+
+            if (currentBit >= 32)
+            {
+                currentInt++;
+                currentBit -= 32;
+                if (currentBit > 0)
+                    bits[currentInt] = bitContainer >> (bitsToAdd - currentBit);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string[] ints = new string[currentInt + 1];
+
+            for (byte i = 0; i < currentInt; i++)
+                ints[i] = IntToBit(i);
+            if (currentBit > 0)
+                ints[currentInt] = Convert.ToString(bits[currentInt], 2).PadLeft(currentBit, '0');
+
+            Array.Reverse(ints);
+
+            return '[' + String.Join(" ", ints) + ']';
+        }
+    }
+}
---
+++ BitSequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitSequence bs = new BitSequence();
+
+            Console.WriteLine(bs);
+            for (int i = 0; i < 8; i++)
+            {
+                bs.AddBits(11, 5);
+                Console.WriteLine(bs);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
