4306dc1456bdc2c85c7530e03e218481a8b7bfd8
Inego <yanis.batura@gmail.com>
2015-03-22 22:58:52 +0600

    Wrote a decent hashing function for BitSequence; started writing benchmarks

--- BitSequence/BitSequence.cs
+++ BitSequence/BitSequence.cs
@@ -6,17 +6,81 @@ using System.Threading.Tasks;
 
 namespace BitSequence
 {
-    class BitSequence
+    class BitSequence : IEquatable<BitSequence>
     {
-        private List<int> bits = new List<int>(1);
+        private static uint[] startprimes = new uint[32] { 1, 0x7FFFFFFF, 0x3FFFFFDD, 0x1FFFFFFD, 0xFFFFFC7, 0x7FFFFD9, 0x3FFFFFB, 0x1FFFFD9, 0xFFFFFD, 0x7FFFF1, 0x3FFFFD, 0x1FFFF7, 0xFFFFD, 0x7FFFF, 0x3FFFB, 0x1FFFF, 0xFFF1, 0x7FED, 0x3FFD, 0x1FFF, 0xFFD, 0x7F7, 0x3FD, 0x1FD, 0xFB, 0x7F, 0x3D, 0x1F, 0xD, 0x7, 0x3, 0x2 };
+
+
+        public List<uint> bits = new List<uint>(1);
         private int currentBit = 0;
+
+
+        public BitSequence()
+        {
+        }
+
+
+        public BitSequence(uint container, byte bits)
+        {
+            this.bits.Add(container);
+            currentBit = bits;
+        }
+
+        public static byte GetBitNumber(int choices)
+        {
+            if (choices < 3)
+                return 1;
+            if (choices < 5)
+                return 2;
+            if (choices < 9)
+                return 3;
+            if (choices < 17)
+                return 4;
+            return 5; // :)
+        }
 
         private string IntToBit(byte pos)
         {
             return Convert.ToString(bits[pos], 2).PadLeft(32, '0');
         }
 
-        public void AddBits(int bitContainer, byte bitsToAdd)
+        public bool Equals(BitSequence other) {
+            return bits.SequenceEqual(other.bits);
+        }
+
+
+        public override bool Equals(Object obj)
+        {
+            BitSequence other = obj as BitSequence;
+            return bits.SequenceEqual(other.bits);
+        }
+
+        public override int GetHashCode()
+        {
+            uint result;
+            int c;
+
+            if (bits.Count == 0)
+                return 0;
+            
+            result = startprimes[currentBit] * (bits[bits.Count - 1] + 1);
+            
+            c = bits.Count - 2;
+
+            while (c >= 0)
+            {
+                unchecked
+                { 
+                    result += bits[c]; 
+                }
+                c--;
+            }
+
+            return unchecked((int)result);
+            
+        }
+
+        public void AddBits(uint bitContainer, byte bitsToAdd)
         {
             if (currentBit == 0)
                 bits.Add(bitContainer);
@@ -45,7 +109,7 @@ namespace BitSequence
 
             Array.Reverse(ints);
 
-            return '[' + String.Join(" ", ints) + ']';
+            return '[' + String.Join(" ", ints) + "] " + GetHashCode().ToString("X8");
         }
     }
 }
--- BitSequence/Program.cs
+++ BitSequence/Program.cs
@@ -3,21 +3,137 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace BitSequence
 {
+    class BitPattern
+    {
+        private byte[][] patternBitsRequired;
+        private int numberOfSamples;
+        private uint[][] samples;
+
+
+        public BitPattern(int numberOfSamples)
+        {
+            this.numberOfSamples = numberOfSamples;
+            
+            samples = new uint[numberOfSamples][];
+            patternBitsRequired = new byte[numberOfSamples][];
+
+            fill();
+        }
+
+        private void fill()
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                int depth = 2 + r.Next(16);
+                samples[i] = new uint[depth];
+                patternBitsRequired[i] = new byte[depth];
+                for (int j = 0; j < depth; j++)
+                {
+                    int choice = 2 + r.Next(12);
+                    samples[i][j] = (uint)r.Next(choice);
+                    patternBitsRequired[i][j] = BitSequence.GetBitNumber(choice);
+                }
+            }
+        }
+
+        public BitSequence[] generateBitSequences()
+        {
+            BitSequence[] result = new BitSequence[numberOfSamples];
+
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                BitSequence b = new BitSequence();
+
+                for (int j = 0; j < samples[i].Length; j++)
+                {
+                    b.AddBits(samples[i][j], patternBitsRequired[i][j]);
+                }
+                
+                result[i] = b;
+            }
+
+            return result;
+        }
+
+
+
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            BitSequence bs = new BitSequence();
 
-            Console.WriteLine(bs);
-            for (int i = 0; i < 8; i++)
+            Console.WriteLine(new BitSequence(2, 31));
+            Console.WriteLine(new BitSequence(1, 30));
+            Console.WriteLine(new BitSequence(5, 0));
+            Console.ReadKey();
+            return;
+
+
+            int samplesCount = 1000000;
+
+            
+
+            Stopwatch s = Stopwatch.StartNew();
+
+            BitPattern p = new BitPattern(samplesCount);
+
+            s.Stop();
+
+            Console.WriteLine("Finished populating bytes. " + s.ElapsedMilliseconds);
+
+            s.Start();
+
+            Console.WriteLine("Started populating bit sequences - " + GC.GetTotalMemory(false).ToString());
+
+            BitSequence[] bs = p.generateBitSequences();
+
+            s.Stop();
+
+            Console.WriteLine("Finished populating bit sequences - " + s.ElapsedMilliseconds + " ms " + GC.GetTotalMemory(false).ToString());
+
+
+
+            // Test collisions
+
+            Console.WriteLine("Starting collision test");
+
+            Dictionary<int, BitSequence> hashDict = new Dictionary<int, BitSequence>(samplesCount);
+            HashSet<BitSequence> fullSet = new HashSet<BitSequence>();
+
+            int hashCollisions = 0;
+            int fullCollisions = 0;
+
+            foreach (BitSequence b in bs)
             {
-                bs.AddBits(11, 5);
-                Console.WriteLine(bs);
+                int hash = b.GetHashCode();
+                bool hashContained = hashDict.ContainsKey(hash);
+                if (!hashContained)
+                    hashDict[hash] = b;
+
+                if (fullSet.Contains(b))
+                    fullCollisions++;
+                else
+                {
+                    if (hashContained)
+                        hashCollisions++;
+                    fullSet.Add(b);
+                }
             }
+
+            Console.WriteLine("  Hash collisions: " + hashCollisions);
+            Console.WriteLine("  Full collisions: " + fullCollisions);
+
+            Console.WriteLine("Finished collision test");
 
             Console.ReadKey();
 
